954ea2a1de71af13541ab1e7014c638dd455b9f4
LiamJTolentino <liamjtolentino@gmail.com>
2026-02-26 16:54:47 -0500

    implemented alg parsing and started on Orozco solver

--- AlgParse.py
+++ AlgParse.py
@@ -1,4 +1,34 @@
-def invert_sequence(alg):
+import re
+
+ALG_PATTERN = r"((\s*([xyzUuLlFfRrBbDdMES]['2]?\s+)+([xyzUuLlFfRrBbDdMES]['2]?\s*)*)|([xyzUuLlFfRrBbDdMES]['2]?))"
+COMM_PATTERN = r"\[\s*" + ALG_PATTERN + r",\s*" + ALG_PATTERN + r"\]"
+CONJ_PATTERN = r"\[\s*" + ALG_PATTERN + r":\s*" + ALG_PATTERN + r"\]"
+
+def simplify(compound_alg) -> str:
+    """Expands an algorithm recursively until it is a simple sequence"""
+    output = compound_alg
+    if is_simple_sequence(compound_alg):
+        return compound_alg.strip()
+    
+    # Expand innermost commutators
+    com_found = re.search(COMM_PATTERN,output)
+    if com_found is not None:
+        comm = com_found.group()
+        output = output.replace(comm,expand_commutator(comm))
+    # Expand innermost conjugates
+    conj_found = re.search(CONJ_PATTERN,output)
+    if conj_found is not None:
+        conj = conj_found.group()
+        output = output.replace(conj,expand_conjugate(conj))
+    
+    return simplify(output)
+
+
+def is_simple_sequence(alg):
+    """Returns true if alg is a simple sequence (e.g. "R U R' U')"""
+    return re.fullmatch(ALG_PATTERN,alg) is not None
+
+def invert_sequence(alg) -> str:
     """Returns the inverse of a basic move sequence (Not for commutators)
     
     Args:
@@ -20,3 +50,19 @@ def invert_sequence(alg):
             inverted_moves.append(move + "'") # Invert normal turns by adding a prime
 
     return " ".join(inverted_moves)
+
+def expand_commutator(commutator) -> str:
+    """Takes a string in commutator notation and expands it to a simple sequence
+        ex. "[D, R U R']" -> "D R U R' D' R U' R"
+    """
+    A,B = commutator.replace("[","").replace("]","").split(',')
+    expanded = f"{A.strip()} {B.strip()} {invert_sequence(A.strip())} {invert_sequence(B.strip())}"
+    return expanded
+
+def expand_conjugate(conjugate) -> str:
+    """Takes a string in conjugate notation and expands it to a simple sequence
+        ex. "[R' U': R' F R F']" -> "R' U' R' F R F' U R"
+    """
+    A,B = conjugate.replace("[","").replace("]","").split(':')
+    expanded = f"{A.strip()} {B.strip()} {invert_sequence(A.strip())}"
+    return expanded
---
+++ orozco_solver.py
@@ -0,0 +1,20 @@
+import re
+import csv
+import os
+import AlgParse as alg
+
+class Orozco:
+    def __init__(self):
+        pass
+
+    def set_edge_letters(self,letters):
+        pass
+
+    def set_corner_letters(self,letters):
+        pass
+
+    def solve(self) -> str:
+        pass
+
+    def load_commutators(csv_path):
+        pass
